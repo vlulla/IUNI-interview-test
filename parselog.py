a8ae59a0f7d54e4962549b710aa404414a41c53e
Vijay Lulla <vijaylulla@gmail.com>
2021-12-04 16:15:58 -0500

    first attempt at parsing the line

--- parselog.py
+++ parselog.py
@@ -3,14 +3,30 @@
 ## Author: Vijay Lulla
 ## Date: 
 ##
-import re,os,sys,datetime,typing,logging
+import re,os,sys,datetime,typing
 import hypothesis as hy, hypothesis.strategies as st
 
 def parseLogLine(line : str) -> dict:
-    return dict()
+    log = {}
+    cols = re.split('[\s"]+', line)
 
-def main() -> None:
-    print(parseLogLine(""))
+    log['datetime'] = f"{cols[0]} {cols[1]}"
+    log['logName'] = f"{cols[2]}"
+    log['logLevel'] = f"{cols[3]}"
+    assert log['logLevel'].upper() in ['CRITICAL', 'ERROR', 'WARNING', 'INFO', 'DEBUG', 'NOTSET']
+    log['ID'] = f"{cols[4]}"
+    log['msg'] = f"{' '.join(cols[5:])}".strip()
+
+    return log
+
+def getLogLines(fname : str) -> typing.List[str]:
+    with open(fname) as fd:
+        dat = fd.read()
+    return [l for l in dat.split("\n") if l != '']
+
+def main(fname : str) -> None:
+    lines = getLogLines(fname)
+    print(parseLogLine(lines[0]))
 
 if __name__ == "__main__":
-    main()
+    main("api.log")
